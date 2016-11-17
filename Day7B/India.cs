e3439296b9c3567b1a633f4aa59703f4ace65e59
iAvinashVarma <avinashvarma@live.com>
2016-11-17 05:38:04 +0530

    Task #4 : Removing unneccessary project files.

---
+++ Day7B/India.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day7B
+{
+    // Using this library to demonstrate GAC (Shared Assembly)
+    public class India
+    {
+        public void GetStates()
+        {
+            Console.WriteLine("AP is one of the Indian states.");
+        }
+    }
+}
