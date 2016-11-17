44fcb00b4ba1acc87a03c6776a71a87edd01fda6
iAvinashVarma <avinashvarma@live.com>
2016-11-17 05:41:15 +0530

    Task #5 : Remove Day7D project.

---
+++ Day7C/India.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day7B
+{
+    // Using this library to demonstrate GAC (Shared Assembly)
+    // Using this library to demonstrate GAC (Shared Assembly)
+    public class India
+    {
+        public void GetStates()
+        {
+            Console.WriteLine("TS is one of the Indian states.");
+        }
+    }
+}
