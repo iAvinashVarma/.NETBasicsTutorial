3afe4a489a99693b5993f5946126977a74fa81d5
iAvinashVarma <avinashvarma@live.com>
2016-11-18 07:41:41 +0530

    Task #8 : Loops, TPL ForEach, File, StopWatch

---
+++ Day9/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Day9
+{
+    internal class Program
+    {
+        private static string[] states = { "Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh", "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jammu & Kashmir", "Jharkhand", "Karnataka", "Kerala", "Madhya Pradesh", "Maharashtra", "Manipur", "Meghalaya", "Mizoram", "Nagaland", "Odisha (Orissa)", "Punjab", "Rajasthan", "Sikkim", "Tamil Nadu", "Telangana (from June 2nd, 2014)", "Tripura", "Uttar Pradesh", "Uttarakhand", "West Bengal" };
+
+        private static void Main(string[] args)
+        {
+            ForDemo();
+            WhileDemo();
+            DoWhileDemo();
+
+            #region ForEach vs Parallel.ForEach performance.
+
+            double forEachTime, parallelForEachTime;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            ForEachDemo();
+            sw.Stop();
+            forEachTime = sw.Elapsed.TotalSeconds;
+            sw.Start();
+            ParallelForEachDemo();
+            sw = new Stopwatch();
+            sw.Stop();
+            parallelForEachTime = sw.Elapsed.TotalSeconds;
+            Console.WriteLine("ForEach time : {0}{1}Parallel ForEach time : {2}", forEachTime, Environment.NewLine, parallelForEachTime);
+
+            #endregion
+
+            ForEachFileDemo();
+            Console.Read();
+        }
+
+        private static void ForDemo()
+        {
+            Console.WriteLine("For Incremental Demo");
+            // Incremental For
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("For Decremental Demo");
+            // Decremental For
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        private static void WhileDemo()
+        {
+            Console.WriteLine("While Demo");
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+
+        private static void DoWhileDemo()
+        {
+            Console.WriteLine("Do While Demo");
+            int i = 0;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            while (i < 10);
+        }
+
+        private static void ForEachDemo()
+        {
+            Console.WriteLine("ForEach");
+            foreach (string state in states)
+            {
+                Console.WriteLine(string.Format("State : {0} | ThreadID : {1}.", state, Thread.CurrentThread.ManagedThreadId));
+            }
+        }
+
+        private static void ParallelForEachDemo()
+        {
+            Console.WriteLine("ParallelForEach Demo");
+            Parallel.ForEach(states, (state) =>
+                {
+                    Console.WriteLine(string.Format("State : {0} | ThreadID : {1}.", state, Thread.CurrentThread.ManagedThreadId));
+                });
+        }
+
+        private static void ForEachFileDemo()
+        {
+            List<string> writeLine = new List<string>();
+            if (File.Exists(@"C:\Temp\Test.log"))
+            {
+                string[] readLines = File.ReadAllLines(@"C:\Temp\Test.log");
+                foreach (string line in readLines)
+                {
+                    writeLine.Add(line);
+                }
+                File.WriteAllLines(@"C:\Temp\TestForEach.log", writeLine);
+            }
+        }
+    }
+}
---
+++ Day9/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General AvinashVarmamation about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the AvinashVarmamation
+// associated with an assembly.
+[assembly: AssemblyTitle("Day9")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AvinashVarma")]
+[assembly: AssemblyProduct("Day9")]
+[assembly: AssemblyCopyright("Copyright © AvinashVarma 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0e7d65a3-3548-4300-8170-e459a5e3b0a9")]
+
+// Version AvinashVarmamation for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
