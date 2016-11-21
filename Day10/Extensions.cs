2316c00427cc5e1e9899eb1f9850e3613c0eeac0
iAvinashVarma <avinashvarma@live.com>
2016-11-21 07:24:50 +0530

    Task #9 : Extension Methods, IEnumerable, Yield Keyword.

---
+++ Day10/Extensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day10
+{
+    internal static class DateTimeExtension
+    {
+        public static int Age(this DateTime dateTime)
+        {
+            var currentAge = DateTime.Now.Year - dateTime.Year;
+            return currentAge;
+        }
+    }
+
+    internal static class GenericExtension
+    {
+        public static bool IsNull<T>(this T source)
+        {
+            return source == null;
+        }
+    }
+
+    internal class Extensions
+    {
+        private static void Main(string[] args)
+        {
+            #region Extension Methods
+
+            DateTime dateTime = DateTime.Parse("01-08-1990");
+            int myAge = dateTime.Age();
+
+            DateTime date = new DateTime();
+            if (!date.IsNull())
+            {
+                // Will not enter if DateTime is null.
+            }
+
+            List<int> someList = null;
+            if (!someList.IsNull())
+            {
+                foreach (var item in someList)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            #endregion Extension Methods
+        }
+    }
+}
---
+++ Day10A/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Day10A
+{
+    internal class Program
+    {
+        private static List<int> myList = Enumerable.Range(1990, 50).ToList();
+
+        private static IEnumerable<int> MoveWithYield(int count)
+        {
+            foreach (var item in myList)
+            {
+                if (item > count)
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        private static IEnumerable<int> MoveNormal(int count)
+        {
+            List<int> tempList = new List<int>();
+            foreach (var item in myList)
+            {
+                if (item > count)
+                {
+                    tempList.Add(item);
+                }
+            }
+            return tempList;
+        }
+
+        private static void Main(string[] args)
+        {
+            double yieldTime, normalTime;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            foreach (var item in MoveWithYield(2000))
+            {
+                Console.WriteLine(item);
+            }
+            sw.Stop();
+            yieldTime = sw.ElapsedMilliseconds;
+
+            sw = new Stopwatch();
+            sw.Start();
+            foreach (var item in MoveNormal(2000))
+            {
+                Console.WriteLine(item);
+            }
+            sw.Stop();
+            normalTime = sw.ElapsedMilliseconds;
+
+            Console.WriteLine("Yield Time : {0} | Normal Time : {1}", yieldTime, normalTime);
+        }
+    }
+}
