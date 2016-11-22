b4bf380f3d7a776e3626c70d8e3e0ec06cc60037
iAvinashVarma <avinashvarma@live.com>
2016-11-22 07:16:57 +0530

    Task #11 : IEnumerable, IEnumerator, Is, As, Delegate, SimpleDelegate Vs MultiCast Delegate.

---
+++ Day11/IEnumerableVsIEnumerator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day11
+{
+    internal class IEnumerableVsIEnumerator
+    {
+        private static void Main(string[] args)
+        {
+            List<int> years = Enumerable.Range(1990, 30).ToList();
+            string country = "India";
+
+            #region IS, AS Cast VS Normal Cast
+
+            IEnumerable<int> ienumerableAsCast = years as IEnumerable<int>;
+            IEnumerable<int> ienumerableNormalCast = (IEnumerable<int>)years;
+
+            if (country is List<int>)
+            {
+                Console.WriteLine("Years is of List");
+            }
+
+            #endregion IS, AS Cast VS Normal Cast
+
+            #region IEnumerable
+
+            // Doesn't store the cursor state.
+            Console.WriteLine("IEnumerable Logic");
+            foreach (var item in ienumerableAsCast)
+            {
+                Console.WriteLine(item);
+            }
+
+            #endregion IEnumerable
+
+            #region IEnumerator
+
+            IEnumerator<int> ienumerator = years.GetEnumerator();
+
+            // This will store the cursor state.
+            Console.WriteLine("IEnumerator Logic");
+            while (ienumerator.MoveNext())
+            {
+                Console.WriteLine(ienumerator.Current);
+            }
+
+            #endregion IEnumerator
+        }
+    }
+}
---
+++ Day11A/Delegates.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Day11A
+{
+    internal class DelegateExample
+    {
+        public static void Addition(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        public static void Multiplication(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+    }
+
+    internal class Delegates
+    {
+        public delegate void MyDelegate();
+
+        public delegate void CalDelegate(int x, int y);
+
+        private static void Main(string[] args)
+        {
+            #region Simple or Multicast Delegate
+
+            // Multiple instances
+            CalDelegate aDel, mDel, amDel, amMinusmDel;
+            aDel = DelegateExample.Addition;
+            mDel = DelegateExample.Multiplication;
+            Console.WriteLine("Simple Delegate : Addition");
+            aDel(4, 4);
+            Console.WriteLine("Simple Delegate : Mutliplication");
+            mDel(4, 4);
+            Console.WriteLine("MultiCast Delegate : Addition + Multiplication");
+            amDel = aDel + mDel;
+            amDel(4, 4);
+            Console.WriteLine("Unlinking MultiCast Delegate : MultiCast - Multiplication");
+            amMinusmDel = amDel - mDel;
+            amMinusmDel(4, 4);
+
+            //Single Instance
+            Console.WriteLine("Single Instance - Delegate");
+            CalDelegate cDel = new CalDelegate(DelegateExample.Addition);
+            cDel += DelegateExample.Multiplication;
+            cDel(9, 9);
+
+            #endregion Simple or Multicast Delegate
+
+            #region Normal
+
+            MyDelegate myThree = new MyDelegate(SomeMethod);
+            myThree.Invoke();
+            MyDelegate myTwo = SomeMethod;
+            myTwo();
+
+            #endregion Normal
+        }
+
+        private static void SomeMethod()
+        {
+            Console.WriteLine("Delegate Example.");
+        }
+    }
+}
